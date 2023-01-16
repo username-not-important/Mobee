51559aabc53a0a1a8d095d129283a76a775ff50e
Name not Important <ac_ali2582@yahoo.com>
2023-01-16 20:42:04 +0330

    Basic Realtime Play/Pause Functionality

--- Mobee.Client.WPF/MainWindow.xaml.cs
+++ Mobee.Client.WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Mobee.Client.WPF
 {
@@ -20,9 +21,65 @@ namespace Mobee.Client.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HubConnection connection;
+        private string Id = new Random().Next(1, 1000).ToString("D4");
+        private bool Status = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7016/PlayersHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0,5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+        
+        private async void _Connect_Click(object sender, RoutedEventArgs e)
+        {
+            connection.On<string, bool>("PlaybackToggled", (user, status) =>
+            {
+                Status = status;
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    _StatusText.Text = status ? "Playing" : "Stopped";
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+
+                _Connect_Button.IsEnabled = false;
+                _Toggle_Button.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                _Connect_Button.IsEnabled = true;
+                _Toggle_Button.IsEnabled = false;
+            }
+        }
+
+        private async void _Toggle_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Status = !Status;
+
+                await connection.InvokeAsync("TogglePlayback", 
+                    $"Player {Id}", Status);
+            }
+            catch (Exception ex)
+            {         
+                _Connect_Button.IsEnabled = true;
+                _Toggle_Button.IsEnabled = false;
+            }
         }
     }
 }
--- Mobee.Server.Aspnet/Hubs/PlayersHub.cs
+++ Mobee.Server.Aspnet/Hubs/PlayersHub.cs
@@ -4,6 +4,9 @@ namespace Mobee.Server.Aspnet.Hubs
 {
     public class PlayersHub : Hub
     {
-
+        public async Task TogglePlayback(string user, bool isPlaying)
+        {
+            await Clients.All.SendAsync("PlaybackToggled", user, isPlaying);
+        }
     }
 }
