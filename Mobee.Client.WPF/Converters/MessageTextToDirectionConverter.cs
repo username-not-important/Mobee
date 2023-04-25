1cb1aa3248136909515bf8feff761753956e302e
Name not Important <ac_ali2582@yahoo.com>
2023-04-25 21:40:38 +0330

    RTL Support in Chat

---
+++ Mobee.Client.WPF/Converters/MessageTextToDirectionConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mobee.Client.WPF.Converters
+{
+    public class MessageTextToDirectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string || string.IsNullOrWhiteSpace(value.ToString()))
+                return FlowDirection.LeftToRight;
+
+            var mostlyRTLChars = (value.ToString() ?? string.Empty).Select(c => c is >= 'ؠ' and <= '৳' ? 1 : 0).Average() > 0.3;
+
+            return mostlyRTLChars ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
